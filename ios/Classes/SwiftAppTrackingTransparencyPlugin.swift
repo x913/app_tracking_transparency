e782ab8f2928d7d0f1ec91f5c66ec383ef848ac8
x913 <x913x913@gmail.com>
2022-06-10 19:11:21 +0500

    fix for ios 15

--- ios/Classes/SwiftAppTrackingTransparencyPlugin.swift
+++ ios/Classes/SwiftAppTrackingTransparencyPlugin.swift
@@ -4,10 +4,14 @@ import AppTrackingTransparency
 import AdSupport
 
 public class SwiftAppTrackingTransparencyPlugin: NSObject, FlutterPlugin {
+
+  var result: Int = -1;
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "app_tracking_transparency", binaryMessenger: registrar.messenger())
     let instance = SwiftAppTrackingTransparencyPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addApplicationDelegate(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -22,6 +26,41 @@ public class SwiftAppTrackingTransparencyPlugin: NSObject, FlutterPlugin {
     }
     else {
         result(FlutterMethodNotImplemented)
+    }
+  }
+
+  public func applicationDidBecomeActive(_ application: UIApplication) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+        self.requestPermission()
+    }
+  } 
+
+  func requestPermission() {
+    if #available(iOS 15.0, *) {
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                switch status {
+                case .authorized:
+                    // Tracking authorization dialog was shown
+                    // and we are authorized
+				print("Authorized")
+				result = 3
+                case .denied:
+                    // Tracking authorization dialog was
+                    // shown and permission is denied
+                    print("Denied")
+				result = 2
+                case .notDetermined:
+                    // Tracking authorization dialog has not been shown
+                    print("Not Determined")
+				result = 0
+                case .restricted:
+                    print("Restricted ")
+				result = 1
+                @unknown default:
+                   result = 0
+                }
+            })
+        }
     }
   }
 
@@ -41,14 +80,27 @@ public class SwiftAppTrackingTransparencyPlugin: NSObject, FlutterPlugin {
     case authorized = 3
   */
   private func requestTrackingAuthorization(result: @escaping FlutterResult) {
-    if #available(iOS 14, *) {
-        ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-            result(Int(status.rawValue))
-        })
+
+    if #available(iOS 15, *) {
+      
+      if(result != -1) {
+        result(result)
+      } else {
+        result(Int(4))
+      }
     } else {
+
+      if #available(iOS 14, *) {
+          ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+            result(Int(status.rawValue))
+        	}) 
+		  } else {
         // return notSupported
         result(Int(4))
+    	}
+
     }
+
   }
 
   private func getAdvertisingIdentifier(result: @escaping FlutterResult) {
